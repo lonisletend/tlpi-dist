bff418c848d0f4e53b80e6f307d73ac58a061200
lonisletend <yangxjsun0324@gmail.com>
2018-05-16 10:51:05 +0800

    Process ID and Parent Process ID

---
+++ 0study/06_Processes/62_PID_and_PPID/get.c
@@ -0,0 +1,11 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+	printf("getpid() = %d\n",(int)getpid());
+	printf("getppid() = %d\n",(int)getppid());
+	return 0;
+
+}
+
