dc42d792842fe13ca9b12928b496d9ab552e0325
lonisletend <yangxjsun0324@gmail.com>
2018-10-20 22:09:09 +0800

    fork stdio buf

---
+++ 0study/24_Process_Creation/fork_stdio_buf.c
@@ -0,0 +1,22 @@
+#include "tlpi_hdr.h"
+
+int main(int argc, char const *argv[])
+{
+    setbuf(stdout, NULL);
+    printf("Hello world\n");
+    write(STDOUT_FILENO, "Ciao\n", 5);
+    /* fflush(stdout); */  //强制刷新缓冲区
+    if(fork() == -1)
+        errExit("fork");
+
+
+    //子进程调用_exit(), 父进程调用exit()
+    /* switch(fork()){
+    case -1:
+        errExit("fork");
+    case 0:
+        _exit(EXIT_SUCCESS);
+    } */
+
+    exit(EXIT_SUCCESS);
+}
