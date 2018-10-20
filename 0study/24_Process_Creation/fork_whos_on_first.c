35872c81cd6b3b3e072966ef26c98dc06a07b26c
lonisletend <yangxjsun0324@gmail.com>
2018-10-20 21:30:26 +0800

    race condition

---
+++ 0study/24_Process_Creation/fork_whos_on_first.c
@@ -0,0 +1,29 @@
+#include <sys/wait.h>
+#include "tlpi_hdr.h"
+
+int main(int argc, char const *argv[])
+{
+    int numChildren, j;
+    pid_t childPid;
+
+    if(argc>1 && strcmp(argv[1], "--help")==0)
+        usageErr("%s [numChildren]\n", argv[0]);
+    numChildren = (argc > 1) ? getInt(argv[1], GN_GT_0, "num-children") : 1;
+    setbuf(stdout, NULL);
+    for(j=0; j<numChildren; j++){
+        switch(childPid = fork()){
+        case -1:
+            errExit("fork");
+        case 0:
+            printf("%d child\n", j);
+            _exit(EXIT_SUCCESS);
+        
+        default:
+            printf("%d parent\n", j);
+            // sleep(3);
+            wait(NULL);
+            break;
+        }
+    }
+    exit(EXIT_SUCCESS);
+}
